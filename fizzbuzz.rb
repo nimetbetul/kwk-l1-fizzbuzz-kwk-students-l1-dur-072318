b6ab36dbe4f366d1341e1cd214dfcf6712926811
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-24 19:07:21 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+# If the number can be divided by 3
+# say "fizz"
+# If the number can be divided by 5
+# say "buzz"
+# If the number can be divided by both 3&5
+# say "fizzbuzz"
+# Otherwise
+# say nothing
+
+# x=14
+# if x % 3 && x % 5 == 0
+#   print "fizzbuzz"
+# elsif x % 3 == 0
+#   print "fizz"
+# elsif x % 5 == 0
+#   print "buzz"  
+# else
+#   print x
+# end
+
